0f94f15e533bc6e5941f9b8f593f600a21803707
MetaTeam91 <144123531+MetaTeam91@users.noreply.github.com>
2024-11-01 08:37:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById('discordBtn').addEventListener('click', function() {
     window.location.href = "https://discord.gg/uBfdarmkQf";
 });
+
+document.getElementById('downloadBtn').addEventListener('click', function() {
+    window.location.href = "https://www.mediafire.com/file/vl9jz8b8ana3ks0/coffi.ipa/file";
+});
